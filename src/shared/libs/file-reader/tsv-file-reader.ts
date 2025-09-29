50b6df664f7d2455181d1ef2d0143bda3e5456d9
art-vasilenko <varayv@list.ru>
2025-09-29 12:50:34 +0300

    Fix import command

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,86 +1,36 @@
+import EventEmitter from 'node:events';
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
-import { RentalOffer, User, City } from '../../types/index.js';
+import { createReadStream } from 'node:fs';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): RentalOffer[] {
-    if (!this.rawData) {
-      throw new Error('File was not read');
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-
-    const cities: Record<string, City> = {
-      Paris : { name: 'Paris', latitude: 48.85661, longitude: 2.351499 },
-      Cologne: { name: 'Cologne', latitude: 50.938361, longitude: 6.959974 },
-      Brussels: { name: 'Brussels', latitude: 50.846557, longitude: 4.351697 },
-      Amsterdam : { name: 'Amsterdam', latitude: 52.370216, longitude: 4.895168 },
-      Hamburg : { name: 'Hamburg', latitude: 53.550341, longitude: 10.000654 },
-      Dusseldorf : { name: 'Dusseldorf', latitude: 51.225402, longitude: 6.776314 },
-    };
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(
-        ([
-          title,
-          description,
-          createdDate,
-          previewImage,
-          cityName,
-          price,
-          firstname,
-          email,
-          avatarPath,
-        ]) => {
-          const user: User = {
-            firstname: firstname.trim(),
-            email: email.trim(),
-            avatarPath: avatarPath?.trim() || 'default-avatar.jpg',
-            password: 'default123', // дефолтный пароль
-            userType: 'ordinary', // дефолтный тип
-          };
-
-          const city: City = cities[cityName] || {
-            name: cityName,
-            latitude: 0,
-            longitude: 0,
-          };
-
-          const offer: RentalOffer = {
-            title: title.trim(),
-            description: description.trim(),
-            postDate: new Date(createdDate),
-            city,
-            previewImage: previewImage.trim(),
-            photos: Array(6).fill(previewImage.trim()),
-            isPremium: false,
-            isFavorite: false,
-            rating: 4.0,
-            propertyType: 'apartment',
-            rooms: 1,
-            guests: 1,
-            price: Number(price),
-            amenities: ['Breakfast', 'Air conditioning'],
-            author: user,
-            commentsCount: 0,
-            coordinates: {
-              latitude: city.latitude,
-              longitude: city.longitude,
-            }
-          };
-
-          return offer;
-        }
-      );
+    this.emit('end', importedRowCount);
   }
 }
