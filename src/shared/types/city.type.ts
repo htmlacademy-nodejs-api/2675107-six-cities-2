358ba0b146bbb3118448989312e88c9f6c977652
art-vasilenko <varayv@list.ru>
2025-11-07 12:51:22 +0300

    End validate create-offer-dto

--- src/shared/types/city.type.ts
+++ src/shared/types/city.type.ts
@@ -1,5 +1,14 @@
 export type CityName = 'Paris' | 'Cologne' | 'Brussels' | 'Amsterdam' | 'Hamburg' | 'Dusseldorf';
 
+export enum CityEnum {
+  Paris = 'Paris',
+  Cologne = 'Cologne',
+  Brussels = 'Brussels',
+  Amsterdam = 'Amsterdam',
+  Hamburg = 'Hamburg',
+  Dusseldorf = 'Dusseldorf',
+}
+
 export type City = {
   name: CityName;
   latitude: number;
